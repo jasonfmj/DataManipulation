7aea35e42b80b86832a63c2e2096461db205d141
jasonfmj <jasonfmj2012@gmail.com>
2022-07-21 13:29:39 -0600

    Update RemoveDuplicates.py

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -1,6 +1,9 @@
+#Created by Jasonfmj.
+#This script is used to Remove Duplicates on an excel .xlsx spreadsheet.
+
 import pandas as pd
 theFile = input("Please enter full path and file to remove duplicates: ")
 theOutput = input("Please enter full path where you want to save the output file: ")
 data = pd.read_excel(r"{}".format(theFile))
-data.drop_duplicates(['app_name', 'application_categories', 'company_name', 'email', 'file_name', 'release_name', 'release_number', 'username'], keep="first", inplace=True)
+data.drop_duplicates(inplace=True)
 data.to_excel(r"{}".format(theOutput) + "\output.xlsx")
